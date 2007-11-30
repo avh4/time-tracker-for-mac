cc11370ed6c8f51e5e227da563c7a0182889ecc6
Aaron VonderHaar <eron@aurora.avh4.net>
2007-11-29 23:12:12 -0800

    Wrote a real unit test for TWorkPeriod.

--- TestTWorkPeriod.h
+++ TestTWorkPeriod.h
@@ -11,6 +11,12 @@
 
 @interface TestTWorkPeriod : SenTestCase {
 
+	NSDate *t0;  // A reference time
+	NSDate *t5s; // A time 5 seconds after t0
+	NSDate *t1m; // A time 1 minute after t0
+	NSDate *t1h1m1s; // A time 1:01:01 after t0
+	NSDate *t5m2hs; // A time 5 minutes and 2.5 seconds after t0
+
 }
 
 @end
--- TestTWorkPeriod.m
+++ TestTWorkPeriod.m
@@ -7,18 +7,38 @@
 //
 
 #import "TestTWorkPeriod.h"
+#import "TWorkPeriod.h"
 
 
 @implementation TestTWorkPeriod
 
-- (void) testCase1
+- (void) setUp
 {
-	;
+	t0 = [NSDate dateWithTimeIntervalSinceReferenceDate:0];
+	t5s = [NSDate dateWithTimeIntervalSinceReferenceDate:5];
+	t1m = [NSDate dateWithTimeIntervalSinceReferenceDate:60];
+	t1h1m1s = [NSDate dateWithTimeIntervalSinceReferenceDate:60*60+60+1];
+	t5m2hs = [NSDate dateWithTimeIntervalSinceReferenceDate:60*5+2.5];
 }
 
-- (void) testCase2
+- (void) testTotalTime
 {
-	STFail(@"No luck");
+	TWorkPeriod *wp;
+	
+	wp = [[TWorkPeriod alloc] init];
+	[wp setStartTime:t0];
+	[wp setEndTime:t5s];
+	STAssertEquals([wp totalTime], 5, @"total time 5s");
+
+	[wp setEndTime:t1m];
+	STAssertEquals([wp totalTime], 60, @"total time 1m");
+
+	[wp setEndTime:t1h1m1s];
+	STAssertEquals([wp totalTime], 61*60+1, @"total time 1h1m1s");
+	
+	// total is rounded down to seconds
+	[wp setEndTime:t5m2hs];
+	STAssertEquals([wp totalTime], 5*60+2, @"total time 5m2.5s");	
 }
 
 @end
