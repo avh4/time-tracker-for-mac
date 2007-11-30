69314f8ef72a8edb6903a14d1bc15c77a5584bb5
Aaron VonderHaar <eron@aurora.avh4.net>
2007-11-29 22:25:41 -0800

    Created dummy unit tests.

---
+++ TestTWorkPeriod.h
@@ -0,0 +1,16 @@
+//
+//  TestTWorkPeriod.h
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 2007-11-29.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestTWorkPeriod : SenTestCase {
+
+}
+
+@end
---
+++ TestTWorkPeriod.m
@@ -0,0 +1,24 @@
+//
+//  TestTWorkPeriod.m
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 2007-11-29.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestTWorkPeriod.h"
+
+
+@implementation TestTWorkPeriod
+
+- (void) testCase1
+{
+	;
+}
+
+- (void) testCase2
+{
+	STFail(@"No luck");
+}
+
+@end
