6b6a78d50432a7471b225146207f143379a757f1
Aaron VonderHaar <eron@aurora.avh4.net>
2007-12-19 12:40:32 -0800

    Create test case TestTimeTrackerDocument testSimpleDocumentCsvExport that contains a single work period.  Interface changes need to be implemented.

--- TestTimeTrackerDocument.m
+++ TestTimeTrackerDocument.m
@@ -18,12 +18,33 @@
 
 - (void) testBlankDocumentCsvExport
 {
-	NSData *expected = [
-		@"Date,Start time,End time,Duration,Project,Task\n"
+	NSData *expected = [@"\
+Date,Start time,End time,Duration,Project,Task\n"
 		dataUsingEncoding:NSASCIIStringEncoding];
+
 	NSError *err = nil;
 	NSData *data = [doc dataOfType:@"CSV" error:&err];
-	// STFail(@"%@", [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding]);
+	STAssertEquals([expected length], [data length], nil);
+	STAssertEqualObjects(expected, data, nil);
+	STAssertNil(err, nil);
+}
+
+- (void) testSimpleDocumentCsvExport
+{
+	NSData *expected = [@"\
+Date,Start time,End time,Duration,Project,Task\n\
+2007-01-03,16:00:00,16:15:00,900,New Project,New Task\n"
+		dataUsingEncoding:NSASCIIStringEncoding];
+	
+	NSDate *d1 = [NSDate dateWithString:@"2007-01-03 16:00:00"];
+	NSDate *d2 = [NSDate dateWithString:@"2007-01-03 16:15:00"];
+	TProject *p = [doc createProject:@"New Project"];
+	TTask *t = [doc createTask:@"New Task" inProject:p];
+	[doc createWorkPeriodInTask:t from:d1 to:d2];
+		
+	NSError *err = nil;
+	NSData *data = [doc dataOfType:@"CSV" error:&err];
+	STAssertEquals([expected length], [data length], nil);
 	STAssertEqualObjects(expected, data, nil);
 	STAssertNil(err, nil);
 }
