3501d78d81924fc8fd604c0aec5eab14f560e10a
Aaron VonderHaar <eron@aurora.avh4.net>
2008-03-11 20:03:09 -0700

    TTAppDelegate: * Stubbed applicationDidFinishLaunching:
    MainMenu.nib:
    * Instantiated TTAppDelegate, and connected it to File's Owner delegate outlet.
    
    Time Tracker.xcodeproj:
    * Added TTAppDelegate.* to the Time Tracker target

---
+++ TTAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TTAppDelegate.h
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 3/11/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TTAppDelegate : NSObject {
+
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification;
+
+@end
---
+++ TTAppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  TTAppDelegate.m
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 3/11/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTAppDelegate.h"
+
+
+@implementation TTAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Create the NSDocumentController and cause it to load the singleton
+	// Time Tracker Document.
+	NSLog(@"TTAppDelegate.applicationDidFinishLaunching:");
+}
+
+@end
