08a0ce2ce25d1978eb61587b8d04540e561b3526
Aaron VonderHaar <eron@aurora.avh4.net>
2008-03-28 14:35:01 -0700

    Case 69: The Time Tracker document window should not be allowed to close.
    TTDocumentWindowDelegate:
    * Created new window delegate with -windowShouldClose:
    
    Time Tracker.xcodeproj:
    * Added TTDocumentWindowDelegate to the Time Tracker target.
    
    TimeTrackerMainWindow.nib:
    * Created an instance of TTDocumentWindowDelegate
    * Connected the instance of TTDocumentWindowDelegate to the Main Window's delegate outlet.

---
+++ TTDocumentWindowDelegate.h
@@ -0,0 +1,18 @@
+//
+//  TTDocumentWindowDelegate.h
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 3/28/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TTDocumentWindowDelegate : NSObject {
+
+}
+
+- (BOOL)windowShouldClose:(id)window;
+
+@end
---
+++ TTDocumentWindowDelegate.m
@@ -0,0 +1,19 @@
+//
+//  TTDocumentWindowDelegate.m
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 3/28/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTDocumentWindowDelegate.h"
+
+
+@implementation TTDocumentWindowDelegate
+
+- (BOOL)windowShouldClose:(id)window
+{
+	return NO;
+}
+
+@end
