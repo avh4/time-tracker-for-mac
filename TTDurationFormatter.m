61e6c180101678f71cbee61f587b3fa75f18b163
Aaron VonderHaar <time-tracker@avh4.net>
2008-04-02 20:59:40 -0700

    Migrated formatting logic from TimeIntervalFormatter to TTDurationFormatter.

--- TTDurationFormatter.m
+++ TTDurationFormatter.m
@@ -14,7 +14,17 @@
 - (NSString *)stringForObjectValue:(id)anObject
 {
 	if ([anObject isKindOfClass:[NSNumber class]]) {
-		return @"Number";
+		NSNumber *aNumber = anObject;
+		int seconds = [aNumber intValue];
+	
+		int hours = seconds / 3600;
+		int minutes = (seconds % 3600) / 60;
+		int secs = seconds % 60;
+		return [NSString stringWithFormat: @"%@%d:%@%d:%@%d",
+			(hours < 10 ? @"0" : @""), hours,
+			(minutes < 10 ? @"0" : @""), minutes,
+			(secs < 10 ? @"0" : @""), secs];
+			
 	} else {
 		return [anObject description];
 	}
