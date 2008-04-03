6cb1340fc9f0a77461afb958e049675fb125b34b
Aaron VonderHaar <time-tracker@avh4.net>
2008-04-02 18:39:05 -0700

    Create a new NSFormatter for time durations.
    Time Tracker.xcodeproj:
    * Add TTDurationFormatter to the Time Tracker target.
    
    TTDurationFormatter: created.

---
+++ TTDurationFormatter.h
@@ -0,0 +1,16 @@
+//
+//  TTDurationFormatter.h
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 4/2/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TTDurationFormatter : NSFormatter {
+
+}
+
+@end
---
+++ TTDurationFormatter.m
@@ -0,0 +1,14 @@
+//
+//  TTDurationFormatter.m
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 4/2/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTDurationFormatter.h"
+
+
+@implementation TTDurationFormatter
+
+@end
