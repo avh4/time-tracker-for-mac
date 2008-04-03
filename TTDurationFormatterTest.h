b7a9903066d12b74a3d724b1bf789b5c72ea523e
Aaron VonderHaar <time-tracker@avh4.net>
2008-04-02 21:08:50 -0700

    Created a testcase, TTDurationFormatterTest.

---
+++ TTDurationFormatterTest.h
@@ -0,0 +1,16 @@
+//
+//  TTDurationFormatterTest.h
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 4/2/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TTDurationFormatterTest : SenTestCase {
+
+}
+
+@end
---
+++ TTDurationFormatterTest.m
@@ -0,0 +1,19 @@
+//
+//  TTDurationFormatterTest.m
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 4/2/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTDurationFormatterTest.h"
+
+
+@implementation TTDurationFormatterTest
+
+- (void)testTrue
+{
+	STAssertTrue(NO, @"Expected TRUE", nil);
+}
+
+@end
