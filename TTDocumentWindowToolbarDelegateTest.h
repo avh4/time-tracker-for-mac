7f18b4fd0888a687b7baa7fb1f0762cdcb0758ee
Aaron VonderHaar <time-tracker@avh4.net>
2008-04-04 17:57:19 -0700

    Created test class for the document window toolbar delegate.
    Time Tracker.xcodeproj:
    * Added TTDocumentWindowToolbarDelegateTest to the Test target.
    
    TTDocumentWindowToolbarDelegateTest:
    * Created.

---
+++ TTDocumentWindowToolbarDelegateTest.h
@@ -0,0 +1,16 @@
+//
+//  TTDocumentWindowToolbarDelegateTest.h
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 4/4/2008.
+//  Copyright 2008 Aaron VonderHaar. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TTDocumentWindowToolbarDelegateTest : SenTestCase {
+
+}
+
+@end
---
+++ TTDocumentWindowToolbarDelegateTest.m
@@ -0,0 +1,14 @@
+//
+//  TTDocumentWindowToolbarDelegateTest.m
+//  Time Tracker
+//
+//  Created by Aaron VonderHaar on 4/4/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTDocumentWindowToolbarDelegateTest.h"
+
+
+@implementation TTDocumentWindowToolbarDelegateTest
+
+@end
