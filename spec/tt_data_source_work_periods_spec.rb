57520efc66814b92a569f4c5f2a0455e97f9d43e
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-22 00:21:14 -0700

    Implement passing test it "should return Start time values".

--- spec/tt_data_source_work_periods_spec.rb
+++ spec/tt_data_source_work_periods_spec.rb
@@ -5,6 +5,9 @@ OSX::ns_import :TTDataSource
 
 describe OSX::TTDataSource do
   
+  TT_WP_COL_DATE = "Date"
+  TT_WP_COL_START_TIME = "StartTime"
+  
   it "should return Work Date values" do
     # Set up constants
     START_TIME = Time.now
@@ -13,7 +16,7 @@ describe OSX::TTDataSource do
     mockWP = mock("TWorkPeriod")
     mockTable = mock("NSTableView")
     mockColumn = mock("NSTableColumn")
-    mockColumn.stub!(:identifier).and_return("Date")
+    mockColumn.stub!(:identifier).and_return(TT_WP_COL_DATE)
     ds = OSX::TTDataSource.alloc.init
     ds.setWorkPeriods( [mockWP] )
     
@@ -28,6 +31,25 @@ describe OSX::TTDataSource do
   end
   
   it "should return Start time values" do
+    # Set up constants
+    START_TIME = Time.now
+    
+    # Set up objects
+    mockWP = mock("TWorkPeriod")
+    mockTable = mock("NSTableView")
+    mockColumn = mock("NSTableColumn")
+    mockColumn.stub!(:identifier).and_return(TT_WP_COL_START_TIME)
+    ds = OSX::TTDataSource.alloc.init
+    ds.setWorkPeriods( [mockWP] )
+    
+    # Set up expectations
+    mockWP.should_receive(:startTime).and_return(START_TIME)
+    
+    # Perform the test
+    date = ds.tableView_objectValueForTableColumn_row(mockTable, mockColumn, 0)
+    
+    # Check assertations
+    date.should == START_TIME.strftime("%H:%M:%S")
   end
   
   it "should return End time values" do
