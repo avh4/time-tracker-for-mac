f6389801c82435f9300c8b6e3e0cbffac1f34fec
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-22 00:23:24 -0700

    Implement passing test "should return End time values".

--- spec/tt_data_source_work_periods_spec.rb
+++ spec/tt_data_source_work_periods_spec.rb
@@ -7,6 +7,7 @@ describe OSX::TTDataSource do
   
   TT_WP_COL_DATE = "Date"
   TT_WP_COL_START_TIME = "StartTime"
+  TT_WP_COL_END_TIME = "EndTime"
   
   it "should return Work Date values" do
     # Set up constants
@@ -53,6 +54,25 @@ describe OSX::TTDataSource do
   end
   
   it "should return End time values" do
+    # Set up constants
+    END_TIME = Time.now
+    
+    # Set up objects
+    mockWP = mock("TWorkPeriod")
+    mockTable = mock("NSTableView")
+    mockColumn = mock("NSTableColumn")
+    mockColumn.stub!(:identifier).and_return(TT_WP_COL_END_TIME)
+    ds = OSX::TTDataSource.alloc.init
+    ds.setWorkPeriods( [mockWP] )
+    
+    # Set up expectations
+    mockWP.should_receive(:endTime).and_return(END_TIME)
+    
+    # Perform the test
+    date = ds.tableView_objectValueForTableColumn_row(mockTable, mockColumn, 0)
+    
+    # Check assertations
+    date.should == END_TIME.strftime("%H:%M:%S")
   end
   
   it "should return Duration values" do
