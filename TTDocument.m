0d9f4ef42ad732a742fe7ffe783e228949a10d23
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-24 18:24:40 -0700

    tt_document_export_csv_spec tests pass.

--- TTDocument.m
+++ TTDocument.m
@@ -37,6 +37,62 @@
 	[_projects removeObject:proj];
 }
 
+- (NSData *)dataOfType:(NSString *)aType error:(NSError **)outError
+{
+	NSMutableData* data = [[[NSMutableData alloc] init] autorelease];
+	
+	NSDateFormatter *dateFormat = [[NSDateFormatter alloc]
+		initWithDateFormat:@"%Y-%m-%d" allowNaturalLanguage:NO];
+	NSDateFormatter *timeFormat = [[NSDateFormatter alloc]
+		initWithDateFormat:@"%H:%M" allowNaturalLanguage:NO];
+	
+	// Write the CSV column headers
+	[data appendData:[@"Date,Start time,End time,Duration (seconds),Project,Task\n" dataUsingEncoding:NSASCIIStringEncoding]];
+
+	NSEnumerator *proje = [_projects objectEnumerator];
+	TProject *proj;
+	while ( proj = [proje nextObject] )
+	{
+		NSEnumerator *taske = [[proj tasks] objectEnumerator];
+		TTask *task;
+		while (task = [taske nextObject])
+		{
+			NSEnumerator *wpe = [[task workPeriods] objectEnumerator];
+			TWorkPeriod *wp;
+			while (wp = [wpe nextObject])
+			{
+				// Date
+				[data appendData:[[dateFormat stringFromDate:[wp startTime]] dataUsingEncoding:NSASCIIStringEncoding]];
+				[data appendData:[@"," dataUsingEncoding:NSASCIIStringEncoding]];
+				// Start time
+				[data appendData:[[timeFormat stringFromDate:[wp startTime]] dataUsingEncoding:NSASCIIStringEncoding]];
+				[data appendData:[@"," dataUsingEncoding:NSASCIIStringEncoding]];
+				// End time
+				[data appendData:[[timeFormat stringFromDate:[wp endTime]] dataUsingEncoding:NSASCIIStringEncoding]];
+				[data appendData:[@"," dataUsingEncoding:NSASCIIStringEncoding]];
+				
+				// Duration (seconds)
+				[data appendData:[[NSString stringWithFormat:@"%d", [wp totalTime]] dataUsingEncoding:NSASCIIStringEncoding]];
+				[data appendData:[@"," dataUsingEncoding:NSASCIIStringEncoding]];
+				
+				// Project
+				[data appendData:[[proj name] dataUsingEncoding:NSASCIIStringEncoding]];
+				[data appendData:[@"," dataUsingEncoding:NSASCIIStringEncoding]];
+				// Task
+				[data appendData:[[task name] dataUsingEncoding:NSASCIIStringEncoding]];
+				[data appendData:[@"\n" dataUsingEncoding:NSASCIIStringEncoding]];
+			}
+		}
+		
+	}
+	
+	[dateFormat release];
+	[timeFormat release];
+
+    return data;
+}
+
+
 @end
 
 // This initialization function gets called when we import the Ruby module.
--- spec/tt_document_export_csv_spec.rb
+++ spec/tt_document_export_csv_spec.rb
@@ -11,7 +11,10 @@ describe OSX::TTDocument do
     data, error = doc.dataOfType_error("CSV")
     error.should be_nil
     
-    data.should equal(%Q{
+    data_string = OSX::NSString.alloc.initWithData_encoding(data, OSX::NSASCIIStringEncoding)
+    
+    data_string.should == (%Q{
+      Date,Start time,End time,Duration (seconds),Project,Task
     }.gsub!(/^\s*/, ''))
   end
   
@@ -20,17 +23,50 @@ describe OSX::TTDocument do
     
     pr1 = mock("Project 1")
     pr2 = mock("Project 2")
+    t11 = mock("Task 1.1")
+    t12 = mock("Task 1.2")
+    t21 = mock("Task 2.1")
+    w111 = MockWorkPeriod.new
+    w112 = MockWorkPeriod.new
+    w121 = MockWorkPeriod.new
+    w211 = MockWorkPeriod.new
+    
+    pr1.stub!(:tasks).and_return( [t11, t12] )
+    pr2.stub!(:tasks).and_return( [t21] )
+    t11.stub!(:workPeriods).and_return( [w111, w112] )
+    t12.stub!(:workPeriods).and_return( [w121] )
+    t21.stub!(:workPeriods).and_return( [w211] )
+    pr1.stub!(:name).and_return("Project 1")
+    pr2.stub!(:name).and_return("Project 2")
+    t11.stub!(:name).and_return("Task 1.1")
+    t12.stub!(:name).and_return("Task 1.2")
+    t21.stub!(:name).and_return("Task 2.1")
+    w111.stub!(:totalTime).and_return( 10*60 )
+    w112.stub!(:totalTime).and_return(  5*60 + 10 )
+    w121.stub!(:totalTime).and_return(  5*60 + 10 )
+    w211.stub!(:totalTime).and_return(  5*60 + 10 )
+    w111.stub!(:startTime).and_return( Time.local(2008,1,1, 10,20,00) )
+    w112.stub!(:startTime).and_return( Time.local(2008,1,1, 10,35,00) )
+    w121.stub!(:startTime).and_return( Time.local(2008,1,3, 10,35,00) )
+    w211.stub!(:startTime).and_return( Time.local(2008,2,4, 10,35,00) )
+    w111.stub!(:endTime).and_return( Time.local(2008,1,1, 10,30,00) )
+    w112.stub!(:endTime).and_return( Time.local(2008,1,1, 10,40,10) )
+    w121.stub!(:endTime).and_return( Time.local(2008,1,3, 10,40,10) )
+    w211.stub!(:endTime).and_return( Time.local(2008,2,4, 10,40,10) )
     
     doc.setProjects( [pr1, pr2] )
     
     data, error = doc.dataOfType_error("CSV")
     error.should be_nil
     
-    data.should equal(%Q{
-      Project 1,Task 1.1,2008-01-01,10:20:00,2008-01-01,10:30:00,00:10:00
-      Project 1,Task 1.1,2008-01-01,10:35:00,2008-01-01,10:40:10,00:05:10
-      Project 1,Task 1.2,2008-01-03,10:35:00,2008-01-03,10:40:10,00:05:10
-      Project 2,Task 2.1,2008-02-04,10:35:00,2008-02-04,10:40:10,00:05:10
+    data_string = OSX::NSString.alloc.initWithData_encoding(data, OSX::NSASCIIStringEncoding)
+    
+    data_string.should == (%Q{
+      Date,Start time,End time,Duration (seconds),Project,Task
+      2008-01-01,10:20,10:30,600,Project 1,Task 1.1
+      2008-01-01,10:35,10:40,310,Project 1,Task 1.1
+      2008-01-03,10:35,10:40,310,Project 1,Task 1.2
+      2008-02-04,10:35,10:40,310,Project 2,Task 2.1
     }.gsub!(/^\s*/, ''))
   end
 
