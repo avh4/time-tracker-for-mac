1ac3b3437b1cf055a00ec9eaf002523f8ec954e7
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-10-08 20:31:28 -0700

    New pending specs for InspectorController.

---
+++ spec/inspector_controller_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe OSX::InspectorController do
+  
+  it "should display the start and end times from the given work period" do
+    pending
+  end
+  
+  it "should display disabled when no work period is set" do
+    pending
+  end
+  
+  it "should update the work period start time" do
+    pending
+  end
+  
+  it "should update the work period end time" do
+    pending
+  end
+  
+end
